0a4b89cf3843fe258b15bb88ccacf46a69b4489f
Sebastian Morr <sebastian@morr.cc>
2018-04-10 17:10:25 +0200

    Still testsrc, but with commented-out uridecodebin

--- src/bin/bla.rs
+++ src/bin/bla.rs
@@ -34,12 +34,17 @@ const HEIGHT: usize = 240;
 
 fn create_pipeline2(appsrc: gst_app::AppSrc) -> Result<gst::Pipeline, Error> {
     let pipeline = gst::Pipeline::new(None);
-    let src =
-        gst::ElementFactory::make("videotestsrc", None).ok_or(MissingElement("videotestsrc"))?;
+    let src = gst::ElementFactory::make("videotestsrc", None).ok_or(MissingElement("videotestsrc"))?;
+    //let src = gst::ElementFactory::make("uridecodebin", None).ok_or(MissingElement("uridecodebin"))?;
+    //src.set_property("uri", &"file:///home/seb/library/movies/Brave/Brave.2012.1080p.BRrip.x264.YIFY.mp4");
+    let convert = gst::ElementFactory::make("videoconvert", None).ok_or(MissingElement("videoconvert"))?;
+    let scale = gst::ElementFactory::make("videoscale", None).ok_or(MissingElement("videoscale"))?;
+    let rate = gst::ElementFactory::make("videorate", None).ok_or(MissingElement("videorate"))?;
     let sink = gst::ElementFactory::make("appsink", None).ok_or(MissingElement("appsink"))?;
 
-    pipeline.add_many(&[&src, &sink])?;
-    src.link(&sink)?;
+    pipeline.add_many(&[&src, &convert, &scale, &rate, &sink])?;
+    //gst::Element::link_many(&[&convert, &scale, &rate, &sink])?;
+    gst::Element::link_many(&[&src, &convert, &scale, &rate, &sink])?;
 
     let appsink = sink.clone()
         .dynamic_cast::<gst_app::AppSink>()
@@ -49,8 +54,54 @@ fn create_pipeline2(appsrc: gst_app::AppSrc) -> Result<gst::Pipeline, Error> {
         .fps(gst::Fraction::new(5, 1))
         .build()
         .expect("Failed to create video info");
-
     appsink.set_caps(&info.to_caps().unwrap());
+
+    //let pipeline_clone = pipeline.clone();
+    //let convert_clone = convert.clone();
+    //src.connect_pad_added(move |_, src_pad| {
+    //    let pipeline = &pipeline_clone;
+    //    let convert = &convert_clone;
+
+    //    println!(
+    //        "Received new pad {} from {}",
+    //        src_pad.get_name(),
+    //        pipeline.get_name()
+    //    );
+
+    //    let sink_pad = convert
+    //        .get_static_pad("sink")
+    //        .expect("Failed to get static sink pad from convert");
+    //    if sink_pad.is_linked() {
+    //        println!("We are already linked. Ignoring.");
+    //        return;
+    //    }
+
+    ////gst::debug_bin_to_dot_file(&pipeline_clone, gst::DebugGraphDetails::ALL, "output");
+
+    //    let new_pad_caps = src_pad
+    //        .get_current_caps()
+    //        .expect("Failed to get caps of new pad.");
+    //    let new_pad_struct = new_pad_caps
+    //        .get_structure(0)
+    //        .expect("Failed to get first structure of caps.");
+    //    let new_pad_type = new_pad_struct.get_name();
+
+    //    let is_audio = new_pad_type.starts_with("video/x-raw");
+    //    if !is_audio {
+    //        println!(
+    //            "It has type {} which is not raw video. Ignoring.",
+    //            new_pad_type
+    //        );
+    //        return;
+    //    }
+
+    //    let ret = src_pad.link(&sink_pad);
+    //    if ret != gst::PadLinkReturn::Ok {
+    //        println!("Type is {} but link failed.", new_pad_type);
+    //    } else {
+    //        println!("Link succeeded (type {}).", new_pad_type);
+    //    }
+    //});
 
     appsink.set_callbacks(
         gst_app::AppSinkCallbacks::new()
@@ -184,30 +235,68 @@ fn main_loop(pipeline: gst::Pipeline, pipeline2: gst::Pipeline) -> Result<(), Er
     pipeline.set_state(gst::State::Playing).into_result()?;
     pipeline2.set_state(gst::State::Playing).into_result()?;
 
-    let bus = pipeline
-        .get_bus()
-        .expect("Pipeline without bus. Shouldn't happen!");
+//    let bus = pipeline
+//        .get_bus()
+//        .expect("Pipeline without bus. Shouldn't happen!");
+//
+    //while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
+    //    use gst::MessageView;
 
-    while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
-        use gst::MessageView;
+    //    match msg.view() {
+    //        MessageView::Eos(..) => break,
+    //        MessageView::Error(err) => {
+    //            pipeline.set_state(gst::State::Null).into_result()?;
+    //            Err(ErrorMessage {
+    //                src: err.get_src()
+    //                    .map(|s| s.get_path_string())
+    //                    .unwrap_or_else(|| String::from("None")),
+    //                error: err.get_error().description().into(),
+    //                debug: err.get_debug(),
+    //                cause: err.get_error(),
+    //            })?;
+    //        }
+    //        _ => (),
+    //    }
+    //}
 
-        match msg.view() {
-            MessageView::Eos(..) => break,
-            MessageView::Error(err) => {
-                pipeline.set_state(gst::State::Null).into_result()?;
-                Err(ErrorMessage {
-                    src: err.get_src()
-                        .map(|s| s.get_path_string())
-                        .unwrap_or_else(|| String::from("None")),
-                    error: err.get_error().description().into(),
-                    debug: err.get_debug(),
-                    cause: err.get_error(),
-                })?;
-            }
-            _ => (),
+    let main_loop = glib::MainLoop::new(None, false);
+    let main_loop_clone = main_loop.clone();
+    let main_loop_clone2 = main_loop.clone();
+    let bus = pipeline.get_bus().unwrap();
+    bus.connect_message(move |_, msg| match msg.view() {
+        gst::MessageView::Error(err) => {
+            let main_loop = &main_loop_clone;
+            eprintln!(
+                "Error received from element {:?}: {}",
+                err.get_src().map(|s| s.get_path_string()),
+                err.get_error()
+                );
+            eprintln!("Debugging information: {:?}", err.get_debug());
+            main_loop.quit();
         }
-    }
+        _ => (),
+    });
+    bus.add_signal_watch();
 
+    let bus2 = pipeline2.get_bus().unwrap();
+    bus2.connect_message(move |_, msg| match msg.view() {
+        gst::MessageView::Error(err) => {
+            let main_loop = &main_loop_clone2;
+            eprintln!(
+                "Error received from element {:?}: {}",
+                err.get_src().map(|s| s.get_path_string()),
+                err.get_error()
+                );
+            eprintln!("Debugging information: {:?}", err.get_debug());
+            main_loop.quit();
+        }
+        _ => (),
+    });
+    bus2.add_signal_watch();
+
+    main_loop.run();
+
+    pipeline2.set_state(gst::State::Null).into_result()?;
     pipeline.set_state(gst::State::Null).into_result()?;
 
     Ok(())
